31ff9755f23b90e4d72f6990a4208ca6dd92dba2
Zakcrust <zakcrusted@gmail.com>
2020-10-09 23:10:11 +0700

    integrate movement system & soul-like character perspective

--- TestEnv/PlayerMovement.gd
+++ TestEnv/PlayerMovement.gd
@@ -20,17 +20,26 @@ func _look_at_mouse() -> void:
 	look_at(get_global_mouse_position())
 
 func _move_by_controller(delta):
-	look_at(position + (velocity * speed))
 	if velocity == Vector2():
-		$Sprite.play("idle")
+		$Body.play("idle")
 	else:
-		$Sprite.play("move")
+		_face_to_position(velocity)
+		$Body.play("move")
 	move_and_collide(velocity * speed * delta)
 
 
 func _on_Controller_send_button_pos(pos):
 	velocity = pos
 	
+
+func _face_to_position(point : Vector2) -> void:
+	print(point)
+	if point.x >= 0:
+		print("RIGHT")
+		_flip_body(true)
+	else:
+		print("LEFT")
+		_flip_body(false)
 
 
 func _on_Controller_send_shoot():
@@ -57,13 +66,21 @@ func _find_target() -> Area2D:
 func _idle_fire():
 	if velocity == Vector2() and not reload:
 		if target != null:
-			look_at(target.position)
+			var target_pos = position.direction_to(target.position)
+			_face_to_position(target_pos)
+			$Body/Hand.look_at(target.position)
 			var new_bullet = bullet.instance()
-			new_bullet.transform = $GunPoint.global_transform
-			new_bullet.position = $GunPoint.global_position
+			new_bullet.transform = $Body/Hand/GunPoint.global_transform
+			new_bullet.position = $Body/Hand/GunPoint.global_position
 			get_parent().add_child(new_bullet)
 			$ReloadTimer.start()
 			reload = true
+
+func _flip_body(cond : bool):
+	if cond:
+		$Body.scale.x = 1
+	else:
+		$Body.scale.x = -1
 
 
 func _on_ReloadTimer_timeout():
